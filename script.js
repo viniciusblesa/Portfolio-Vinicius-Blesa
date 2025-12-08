933db190e3fb568347bc354171ad68e9ca4c88fe
Vinícius Blesa dos Santos <blesa1269@gmail.com>
2025-12-08 09:04:16 -0300

    Atualização de Identação

--- script.js
+++ script.js
@@ -41,28 +41,28 @@ btnTopo.addEventListener('click', () => {
 });
 
 // Formulario
-    const form = document.getElementById('formContato');
-    const inputs = form.querySelectorAll('input, textarea');
-    const botao = document.getElementById('btnEnviar');
-    const mensagemSucesso = document.getElementById('mensagemSucesso');
+const form = document.getElementById('formContato');
+const inputs = form.querySelectorAll('input, textarea');
+const botao = document.getElementById('btnEnviar');
+const mensagemSucesso = document.getElementById('mensagemSucesso');
 
-    // Habilita o botão apenas quando todos os campos estiverem válidos
-    form.addEventListener('input', () => {
-        const todosPreenchidos = Array.from(inputs).every(input => input.checkValidity());
-        botao.disabled = !todosPreenchidos;
-    });
+// Habilita o botão apenas quando todos os campos estiverem válidos
+form.addEventListener('input', () => {
+    const todosPreenchidos = Array.from(inputs).every(input => input.checkValidity());
+    botao.disabled = !todosPreenchidos;
+});
 
-    // "Finge" que enviou
-    form.addEventListener('submit', (e) => {
-        e.preventDefault(); // impede o envio real
-        botao.disabled = true;
-        botao.textContent = "Enviando...";
+// "Finge" que enviou
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // impede o envio real
+    botao.disabled = true;
+    botao.textContent = "Enviando...";
 
-        // Simula um pequeno tempo de processamento
-        setTimeout(() => {
-            mensagemSucesso.style.display = "block";
-            botao.textContent = "Enviar";
-            form.reset();
-            botao.disabled = true; // desativa novamente até preencher tudo
-        }, 1500);
-    });
+    // Simula um pequeno tempo de processamento
+    setTimeout(() => {
+        mensagemSucesso.style.display = "block";
+        botao.textContent = "Enviar";
+        form.reset();
+        botao.disabled = true; // desativa novamente até preencher tudo
+    }, 1500);
+});
